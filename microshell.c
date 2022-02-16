105ae97d4405d89ed06c81ad9db4158a5c2552db
Kamil Przybyl <kamil776170@gmail.com>
2022-02-16 13:49:46 +0100

    looks better now

--- microshell.c
+++ microshell.c
@@ -15,22 +15,56 @@ void	exec_cd(char **argv, int i) {
 
 	if (i != 2) {
 		write(2, "cd: too many arguments\n", 23);
-		// return 1;
 	}
 	else if (chdir(argv[1]) != 0) {
 		write(2, "cd: no such file or directory : ", 32);
 		write(2, argv[1], strlen(argv[1]));
 		write(2, "\n", 1);
-		// return 1;
 	}
-	// return 0;
+}
+
+void	exec_to_stdout(int tmp_fd, char **argv, int n, char **envp) {
+
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0) {
+		dup2(tmp_fd, STDIN_FILENO);
+		exec_bin(argv, n, tmp_fd, envp);
+	}
+	else {
+		close(tmp_fd);
+		wait(NULL);
+		tmp_fd = dup(STDIN_FILENO);
+	}
+}
+
+void	exec_to_pipe(int tmp_fd, char **argv, int n, char **envp) {
+
+	pid_t	pid;
+	int		fd[2];
+
+	pipe(fd);
+	pid = fork();
+	if (pid == 0) {
+		dup2(tmp_fd, STDIN_FILENO);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		exec_bin(argv, n, tmp_fd, envp);
+	}
+	else {
+		close(fd[1]);
+		close(tmp_fd);
+		wait(NULL);
+		tmp_fd = dup(fd[0]);
+		close(fd[0]);
+	}
 }
 
 int	main(__attribute__ ((unused)) int argc, char **argv, char **envp) {
 
 	int		i;
-	pid_t	pid;
-	int		fd[2];
 	int		tmp_fd;
 
 	i = 0;
@@ -40,37 +74,14 @@ int	main(__attribute__ ((unused)) int argc, char **argv, char **envp) {
 		argv = &argv[i + 1];
 		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
 			i++;
-		if (strcmp(argv[0], "cd") == 0)
+		if (strcmp(argv[0], "cd") == 0) {
 			exec_cd(argv, i);
-		else if (argv != &argv[i] && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) {
-			pid = fork();
-			if (pid == 0) {
-				dup2(tmp_fd, STDIN_FILENO);
-				exec_bin(argv, i, tmp_fd, envp);
-			}
-			else {
-				close(tmp_fd);
-				wait(NULL);
-				tmp_fd = dup(STDIN_FILENO);
-			}
 		}
-		else if(argv != &argv[i] && strcmp(argv[i], "|") == 0) {
-			pipe(fd);
-			pid = fork();
-			if (pid == 0) {
-				dup2(tmp_fd, STDIN_FILENO);
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[0]);
-				close(fd[1]);
-				exec_bin(argv, i, tmp_fd, envp);
-			}
-			else {
-				close(fd[1]);
-				close(tmp_fd);
-				wait(NULL);
-				tmp_fd = dup(fd[0]);
-				close(fd[0]);
-			}
+		else if (argv != &argv[i] && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) {
+			exec_to_stdout(tmp_fd, argv, i, envp);
+		}
+		else if (argv != &argv[i] && strcmp(argv[i], "|") == 0) {
+			exec_to_pipe(tmp_fd, argv, i, envp);
 		}
 	}
 	close(tmp_fd);
